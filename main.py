ea0bcf68c4fd8fd7e04a27b6ee63420ab17e403a
aaronktaey <aaronktaey@gmail.com>
2022-03-16 21:50:56 +0900

    Python-Challenge2

--- main.py
+++ main.py
@@ -1,33 +1,148 @@
 """
-As you can see, the code is broken.
-Create the missing functions, use default arguments.
-Sometimes you have to use 'return' and sometimes you dont.
-Start by creating the functions
+Again, the code is broken, you need to create 4 functions.
+  - add_to_dict: Add a word to a dict.
+  - get_from_dict: Get a word from inside a dict.
+  - update_word: Update a word inside of the dict.
+  - delete_from_dict: Delete a word from the dict.
+
+All this functions should check for errors, follow the comments to see all cases you need to cover.
+
+There should be NO ERRORS from Python in the console.
 """
-def is_on_list(days, i_day):
-    return i_day in days
-
-def get_x(days,x):
-    return days[x]
-
-def add_x(days,x):
-    return days.append(x)
-
-def remove_x(days, x):
-    return days.remove(x)
-# \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
-
-days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-
-print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
-
-print("The fourth item in 'days' is:", get_x(days, 3))
-
-add_x(days, "Sat")
-print(days)
-
-remove_x(days, "Mon")
-print(days)
 
 
-# /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
+def add_to_dict(dictionary={}, word="", definition=""):
+  if not isinstance(dictionary,dict):
+    print(f"You need to send a dictionary. You sent: {type(dictionary)}")
+  elif word == "" or definition == "":
+        print("You need to send a word and a definition.")
+  elif word in dictionary:
+    print(f"{word} is already on the dictionary. Won't add.")
+  else:
+    dictionary[word] = definition
+    print(f"{word} has been added.")
+  
+def get_from_dict(dictionary={}, word=""):
+  if not isinstance(dictionary,dict):
+    print(f"You need to send a dictionary. You sent: {type(dictionary)}")
+  elif word == "":
+        print("You need to send a word to search for.")
+  elif word not in dictionary:
+    print(f"{word} was not found in this dict.")
+  else:
+    print(f"{word} : {dictionary[word]}")
+
+def update_word(dictionary={}, word="", definition=""):
+  if not isinstance(dictionary,dict):
+    print(f"You need to send a dictionary. You sent: {type(dictionary)}")
+  elif word == "" or definition == "":
+        print("You need to send a word and a definition to update.")
+  elif word not in dictionary:
+    print(f"{word} is not on the dict. Can't update non-existing word.")
+  else:
+    dictionary[word] = definition
+    print(f"{word} has been updated to: {dictionary[word]}")
+
+def delete_from_dict(dictionary={}, word=""):
+  if not isinstance(dictionary,dict):
+    print(f"You need to send a dictionary. You sent: {type(dictionary)}")
+  elif word == "":
+        print("You need to specify a word to delete.")
+  elif word not in dictionary:
+    print(f"{word} is not in this dict. Won't delete.")
+  else:
+    dictionary.pop(word)
+    print(f"{word} is deleted.")
+
+# \/\/\/\/\/\/\ DO NOT TOUCH  \/\/\/\/\/\/\
+
+import os
+
+os.system('clear')
+
+
+my_english_dict = {}
+
+print("\n###### add_to_dict ######\n")
+
+# Should not work. First argument should be a dict.
+print('add_to_dict("hello", "kimchi"):')
+add_to_dict("hello", "kimchi")
+
+# Should not work. Definition is required.
+print('\nadd_to_dict(my_english_dict, "kimchi"):')
+add_to_dict(my_english_dict, "kimchi")
+
+# Should work.
+print('\nadd_to_dict(my_english_dict, "kimchi", "The source of life."):')
+add_to_dict(my_english_dict, "kimchi", "The source of life.")
+
+# Should not work. kimchi is already on the dict
+print('\nadd_to_dict(my_english_dict, "kimchi", "My fav. food"):')
+add_to_dict(my_english_dict, "kimchi", "My fav. food")
+
+
+print("\n\n###### get_from_dict ######\n")
+
+# Should not work. First argument should be a dict.
+print('\nget_from_dict("hello", "kimchi"):')
+get_from_dict("hello", "kimchi")
+
+# Should not work. Word to search from is required.
+print('\nget_from_dict(my_english_dict):')
+get_from_dict(my_english_dict)
+
+# Should not work. Word is not found.
+print('\nget_from_dict(my_english_dict, "galbi"):')
+get_from_dict(my_english_dict, "galbi")
+
+# Should work and print the definiton of 'kimchi'
+print('\nget_from_dict(my_english_dict, "kimchi"):')
+get_from_dict(my_english_dict, "kimchi")
+
+print("\n\n###### update_word ######\n")
+
+# Should not work. First argument should be a dict.
+print('\nupdate_word("hello", "kimchi"):')
+update_word("hello", "kimchi")
+
+# Should not work. Word and definiton are required.
+print('\nupdate_word(my_english_dict, "kimchi"):')
+update_word(my_english_dict, "kimchi")
+
+# Should not work. Word not found.
+print('\nupdate_word(my_english_dict, "galbi", "Love it."):')
+update_word(my_english_dict, "galbi", "Love it.")
+
+# Should work.
+print('\nupdate_word(my_english_dict, "kimchi", "Food from the gods."):')
+update_word(my_english_dict, "kimchi", "Food from the gods.")
+
+# Check the new value.
+print('\nget_from_dict(my_english_dict, "kimchi"):')
+get_from_dict(my_english_dict, "kimchi")
+
+
+print("\n\n###### delete_from_dict ######\n")
+
+# Should not work. First argument should be a dict.
+print('\ndelete_from_dict("hello", "kimchi"):')
+delete_from_dict("hello", "kimchi")
+
+# Should not work. Word to delete is required.
+print('\ndelete_from_dict(my_english_dict):')
+delete_from_dict(my_english_dict)
+
+# Should not work. Word not found.
+print('\ndelete_from_dict(my_english_dict, "galbi"):')
+delete_from_dict(my_english_dict, "galbi")
+
+# Should work.
+print('\ndelete_from_dict(my_english_dict, "kimchi"):')
+delete_from_dict(my_english_dict, "kimchi")
+
+# Check that it does not exist
+print('\nget_from_dict(my_english_dict, "kimchi"):')
+get_from_dict(my_english_dict, "kimchi")
+
+# \/\/\/\/\/\/\ END DO NOT TOUCH  \/\/\/\/\/\/\
